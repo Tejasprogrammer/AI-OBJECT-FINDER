9cb1f56a28897064e2f83cfed51027729f59e6fd
Tejasprogrammer <78012113+Tejasprogrammer@users.noreply.github.com>
2022-10-28 15:06:54 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,29 +1,58 @@
-status="";
+Status = "";
+objects = [];
 
-function setup()
-{
-    canvas=createCanvas(480,350);
-    canvas.center();
-    video=createCapture(VIDEO);
+function setup(){
+    canvas = createCanvas(300,290);
+    canvas.position(480,250);
+    video = createCapture(VIDEO);
+    video.size(300,290);
     video.hide();
 }
+function start(){
+    object_Detector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Object";
+    input_text = document.getElementById("input_id").value;
+}
+function modelLoaded(){
+    console.log("Model_Loaded");
+    Status = true;
+}
+function draw(){
+    image(video,0,0,300,290);
+    if(Status != ""){
+        object_Detector.detect(video, gotResults);
+        for(i = 0;i < objects.length;i++){
+            document.getElementById("status").innerHTML = "Status : Object Detected";
+            console.log(objects.length);
+            fill("#ff0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%",objects[i].x + 15,objects[i].y + 15);
+            noFill();
+            stroke("#ff0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
 
-function start()
-{
-    objectDetector=ml5.objectDetector('cocossd', modelloaded);
-    document.getElementById("status").innerHTML="Status: Detecting Objects";
-
-input=document.getElementById("object_input").value;
-
+            if(objects[i].label == input_text){
+                video.stop();
+                object_Detector.detect(gotResults);
+                document.getElementById("object_found").innerHTML = input_text+" Found";
+                var synth = window.speechSynthesis;
+                var utterThis = new SpeechSynthesisUtterance(input_text + "Found");
+                synth.speak(utterThis);
+            }
+            else{
+                document.getElementById("object_found").innerHTML = input_text + " Not Found";
+            }
+        }
+    }
 }
 
-function modelloaded()
+function gotResults(error,results)
 {
-    console.log("ModelLoaded");
-    status=true;
-}
-
-function draw()
-{
-    image(video,0,0, 480,350);
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        objects = results;
+    }
 }
